074f82e8c2efe9b03815dad155171ea700059f72
caiyilin <919539304@qq.com>
2019-07-11 18:34:58 +0800

    搜索界面，组件布局

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,3 +1,16 @@
+const app = getApp()
+const util = require('../../utils/utils.js')
 Page({
-  
+  data: {
+    value: '',
+  },
+  onChange(e) {
+    this.setData({
+      value: e.detail.value,
+    })
+    console.log(this.data.value)
+  },
+  onFocus(e) {
+    console.log('onFocus', e)
+  }
 })

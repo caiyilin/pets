d3bf134dbbc4951a3d7e6d42ec2c24ea036e1e7e
caiyilin <919539304@qq.com>
2019-07-12 11:21:34 +0800

    list改为模板

--- pages/search/search.js
+++ pages/search/search.js
@@ -3,6 +3,16 @@ const util = require('../../utils/utils.js')
 Page({
   data: {
     value: '',
+    list: [{
+      title: "平安客服邀约",
+      type: "邀约",
+      totalNum: 99,
+      calledNum: 22,
+      uncallNum: 77,
+      belongUser:"王琴",
+      belongProjectName:"销售顾问",
+      batchCreatedUser:"蔡"
+    }]
   },
   onChange(e) {
     this.setData({

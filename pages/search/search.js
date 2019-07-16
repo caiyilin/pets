5383aaad2676a434199544a3d0f279233ef78d5f
caiyilin <919539304@qq.com>
2019-07-16 10:37:00 +0800

    组件

--- pages/search/search.js
+++ pages/search/search.js
@@ -9,10 +9,30 @@ Page({
       totalNum: 99,
       calledNum: 22,
       uncallNum: 77,
-      belongUser:"王琴",
-      belongProjectName:"销售顾问",
-      batchCreatedUser:"蔡"
-    }]
+      belongUser: "王琴",
+      belongProjectName: "销售顾问",
+      batchCreatedUser: "蔡"
+    }],
+
+    value1: [],
+    value2: [],
+    value3: [],
+    value4: [],
+    value5: [],
+    value6: [],
+    value7: [],
+    value8: [],
+    value9: [],
+    displayValue1: '请选择',
+    displayValue2: '请选择',
+    displayValue3: '请选择',
+    displayValue4: '请选择',
+    displayValue5: '请选择',
+    displayValue6: '请选择',
+    displayValue7: '请选择',
+    displayValue8: '请选择',
+    displayValue9: '请选择',
+    lang: 'zh_CN',
   },
   onChange(e) {
     this.setData({
@@ -22,5 +42,43 @@ Page({
   },
   onFocus(e) {
     console.log('onFocus', e)
-  }
+  },
+
+  onChange(e) {
+    console.log(e)
+    const {
+      key,
+      values
+    } = e.detail
+    const lang = values[key]
+
+    this.setData({
+      lang,
+    })
+  },
+  setValue(values, key, mode) {
+    this.setData({
+      [`value${key}`]: values.value,
+      [`displayValue${key}`]: values.label,
+      // [`displayValue${key}`]: values.displayValue.join(' '),
+    })
+  },
+  onConfirm(e) {
+    const {
+      index,
+      mode
+    } = e.currentTarget.dataset
+    this.setValue(e.detail, index, mode)
+    console.log(`onConfirm${index}`, e.detail)
+  },
+  onVisibleChange(e) {
+    this.setData({
+      visible: e.detail.visible
+    })
+  },
+  onClick() {
+    this.setData({
+      visible: true
+    })
+  },
 })
